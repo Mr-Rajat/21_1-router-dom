3187221ee9dfe5febcd3dd4c81f5e3c2d1a9b98f
Rajat Choudhary <rajatch133@gmail.com>
2023-08-31 23:30:20 +0530

    routing by jsx code with createRoutesFromElements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  RouterProvider,
+  Route 
+} from 'react-router-dom';
+
 import Home from './pages/Home';
 import Products from './pages/Products';
 
-const router = createBrowserRouter([
-  { path: '/', element: <Home/> },
-  { path: '/products', element: <Products />}
-]);
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<Home />} />
+    <Route path="/products" element={<Products/>}/>
+  </Route>
+);
+
+const router = createBrowserRouter(routeDefinitions);
+
+// const router = createBrowserRouter([
+//   { path: '/', element: <Home/> },
+//   { path: '/products', element: <Products />}
+// ]);
 
 function App() {
   return <RouterProvider router={router} />
