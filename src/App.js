8bd4ae5a972b4b24dc2e61c214c57a2aebff573b
Rajat Choudhary <rajatch133@gmail.com>
2023-09-01 00:03:33 +0530

    addded navbar. using Root.js and MainNavigation.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { 
-  createBrowserRouter, 
+import {
+  createBrowserRouter,
   // createRoutesFromElements, 
   RouterProvider,
-  Route 
+  // Route 
 } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Products from './pages/Products';
+import RootLayout from './pages/Root';
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -18,12 +19,19 @@ import Products from './pages/Products';
 // const router = createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
-  { path: '/', element: <Home/> },
-  { path: '/products', element: <Products />}
+  {
+    path: '/', element: <RootLayout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/products', element: <Products /> }
+    ],
+  },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <RouterProvider router={router} />
+  )
 }
 
 export default App;
